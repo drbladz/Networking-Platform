cfe444f7805e0aeebf73d8f49266cca9d6b1abd1
A-Seror <91556098+A-Seror@users.noreply.github.com>
2023-03-27 23:17:35 -0400

    Update index.js
    #87 comment the code

--- linkedin-clone/src/reducers/index.js
+++ linkedin-clone/src/reducers/index.js
@@ -1,10 +1,10 @@
 import {combineReducers} from "redux"
 import jobPostingsReducer from "./jobPostingsReducer.js"
 import userReducer from "./userReducer.js"
-
+// Create a rootReducer by combining two different reducers called "userReducer" and "jobPostingsReducer"
 const rootReducer = combineReducers({
   userState: userReducer,
   jobPostingsState: jobPostingsReducer
 })
-
+// Export the rootReducer to be used in other parts of the application
 export default rootReducer
