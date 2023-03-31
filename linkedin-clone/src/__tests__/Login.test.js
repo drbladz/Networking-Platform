610980970b6ee2ddef46c2af31c8e01d8ecc7254
A-Seror <91556098+A-Seror@users.noreply.github.com>
2023-03-31 10:21:54 -0400

    #87 added comments to logintest.js
    #87 added comments to logintest.js

--- linkedin-clone/src/__tests__/Login.test.js
+++ linkedin-clone/src/__tests__/Login.test.js
@@ -2,11 +2,13 @@ import { render, screen } from '@testing-library/react';
 import {Provider} from "react-redux"
 import store from "../store"
 import Login from "../components/Login"
-
+// Begin test case
 test('renders the Login component', () => {
+    // Render Login component with Provider and store
   render(<Provider store={store}>
     <Login />
   </Provider>);
+       // Assert that the text "Welcome to your professional community" appears in the rendered component
   const linkElement = screen.getByText("Welcome to your professional community");
   expect(linkElement).toBeInTheDocument();
 });
