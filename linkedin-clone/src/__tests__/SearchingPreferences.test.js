6f4ad8a26ba2b80cadc4c7cdfdea0bd480c3384f
nicotowa <nicolastowakamgne@gmail.com>
2023-04-07 12:43:49 -0400

    sprint 4 testing

--- linkedin-clone/src/__tests__/SearchingPreferences.test.js
+++ linkedin-clone/src/__tests__/SearchingPreferences.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react';
 import SearchingPreferences from '../components/SearchingPreferences';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
@@ -76,3 +76,106 @@ describe('SearchingPreferences component', () => {
     expect(currentPreferencesText).toHaveTextContent('No preferences set.');
   });
 });
+*/
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import '../firebase';
+import { getFirestore, collection, doc, getDoc, setDoc, updateDoc, deleteField } from 'firebase/firestore';
+
+import SearchingPreferences from '../components/SearchingPreferences';
+
+// Use mock data for authenticated user
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(() => ({
+    currentUser: {
+      uid: 'testUid'
+    }
+  }))
+}));
+
+// Use mock data for Firestore
+jest.mock('firebase/firestore', () => ({
+  collection: jest.fn(() => ({
+    doc: jest.fn(() => ({
+      set: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+      get: jest.fn(() => ({
+        exists: true,
+        data: jest.fn(() => ({
+          searchingPreferences: {
+            jobType: 'full-time',
+            industry: 'technology',
+            experienceLevel: 'senior',
+            remoteWorkOption: 'yes'
+          }
+        }))
+      }))
+    }))
+  }))
+}));
+
+describe('SearchingPreferences', () => {
+  test('renders the component with no preferences set', () => {
+    render(<SearchingPreferences />);
+
+    const preferencesText = screen.getByText('No preferences set.');
+    expect(preferencesText).toBeInTheDocument();
+  });
+
+  test('renders the component with existing preferences', async () => {
+    render(<SearchingPreferences />);
+
+    await waitFor(() => {
+      const jobType = screen.getByText('Job Type:');
+      const industry = screen.getByText('Industry / Sector:');
+      const experienceLevel = screen.getByText('Experience Level:');
+      const remoteWorkOption = screen.getByText('Remote Work Options:');
+
+      expect(jobType).toHaveTextContent('full-time');
+      expect(industry).toHaveTextContent('technology');
+      expect(experienceLevel).toHaveTextContent('senior');
+      expect(remoteWorkOption).toHaveTextContent('yes');
+    });
+  });
+
+  test('updates preferences and shows success message', async () => {
+    render(<SearchingPreferences />);
+
+    // Set up form with new preferences
+    const jobTypeInput = screen.getByLabelText('Job Type');
+    const industryInput = screen.getByLabelText('Industry / Sector');
+    const experienceLevelInput = screen.getByLabelText('Experience Level');
+    const remoteWorkOptionInput = screen.getByLabelText('Remote Work Options');
+
+    fireEvent.change(jobTypeInput, { target: { value: 'part-time' } });
+    fireEvent.change(industryInput, { target: { value: 'finance' } });
+    fireEvent.change(experienceLevelInput, { target: { value: 'entry-level' } });
+    fireEvent.change(remoteWorkOptionInput, { target: { value: 'no' } });
+
+    // Submit form and wait for success message
+    fireEvent.submit(screen.getByRole('button', { name: 'Save Preferences' }));
+
+    const successMessage = await screen.findByText('Searching preferences updated!');
+    expect(successMessage).toBeInTheDocument();
+
+    // Check that new preferences are displayed
+    await waitFor(() => {
+      const jobType = screen.getByText('Job Type:');
+      const industry = screen.getByText('Industry / Sector:');
+      const experienceLevel = screen.getByText('Experience Level:');
+      const remoteWorkOption = screen.getByText('Remote Work Options:');
+
+      expect(jobType).toHaveTextContent('part-time');
+      expect(industry).toHaveTextContent('finance');
+      expect(experienceLevel).toHaveTextContent('entry-level');
+      expect(remoteWorkOption).toHaveTextContent('no');
+    });
+  });
+
+})
--- linkedin-clone/src/__tests__/UpdatePhoto.test.js
+++ linkedin-clone/src/__tests__/UpdatePhoto.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { shallow } from 'enzyme';
 import UpdatePhoto from '../components/UpdatePhoto';
 
@@ -54,4 +54,44 @@ describe('UpdatePhoto', () => {
     expect(updateDocMock).toHaveBeenCalled();
     expect(updateProfilePictureMock).toHaveBeenCalledWith({ ...user, photoURL: 'newUrl' });
   });
+}); */
+
+import { render, screen, fireEvent } from "@testing-library/react";
+import UpdatePhoto from "../components/UpdatePhoto";
+import { Provider } from "react-redux";
+
+describe("UpdatePhoto component", () => {
+  test.only("renders file input and button", () => {
+    render(<UpdatePhoto />);
+    const fileInput = screen.getByLabelText(/file upload/i);
+    expect(fileInput).toBeInTheDocument();
+    const uploadButton = screen.getByRole("button", { name: /update photo/i });
+    expect(uploadButton).toBeInTheDocument();
+  });
+
+  test("updates user profile picture on button click", async () => {
+    const mockDispatch = jest.fn();
+    const mockUser = {
+      uid: "test-user-id",
+      photoURL: "https://example.com/test.jpg",
+    };
+    const { getByLabelText, getByRole } = render(
+      <UpdatePhoto
+        user={mockUser}
+        updateProfilePicture={mockDispatch}
+        userId="test-user-id"
+      />
+    );
+
+    const file = new File(["(⌐□_□)"], "test.jpg", { type: "image/jpeg" });
+    fireEvent.change(getByLabelText(/file upload/i), { target: { files: [file] } });
+    fireEvent.click(getByRole("button", { name: /update photo/i }));
+
+    expect(mockDispatch).toHaveBeenCalledTimes(1);
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: "UPDATE_PROFILE_PICTURE",
+      payload: { uid: "test-user-id", photoURL: expect.any(String) },
+    });
+  });
 });
+
