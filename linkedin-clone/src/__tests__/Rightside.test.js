d5e3ea50edba6c902baea04d9fd5086036346b83
Marckajjo96 <123581453+Marckajjo96@users.noreply.github.com>
2023-04-22 11:54:33 -0400

    Update Rightside.test.js

--- linkedin-clone/src/__tests__/Rightside.test.js
+++ linkedin-clone/src/__tests__/Rightside.test.js
@@ -1,43 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Rightside from '../components/Rightside';
-import { shallow } from "enzyme";
-import { Provider } from 'react-redux';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+jest.mock('react-redux', () => ({
+  connect: () => (ReactComponent) => ReactComponent,
+}));
 
-import configureStore from 'redux-mock-store';
-
-/*test('renders the Rightside component', () => {
-  render(
-    <Rightside />);
-  const linkElement = screen.getByText("Add to your feed");
+Enzyme.configure({ adapter: new Adapter() })
+test('renders the Rightside component', () => {
+  const component = render(<Rightside />);
+  const linkElement = component.getByText("Add to your feed");
   expect(linkElement).toBeInTheDocument();
-});*/
-
-describe('Rightside component', () => {
-  const mockStore = configureStore([]);
-  const store = mockStore({
-    userState: {
-      user: {
-        name: 'Test User',
-        email: 'test@test.com',
-      },
-    },
-  });
-
-  beforeEach(() => {
-    render(
-      <Provider store={store}>
-        <Rightside />
-      </Provider>
-    );
-  });
-
-  test('renders feed list', () => {
-    const feedListElement = screen.getByText('#Jobshare');
-    expect(feedListElement).toBeInTheDocument();
-  });
-
-  test('renders banner image', () => {
-    const bannerImageElement = screen.getByText('#Video');
-    expect(bannerImageElement).toBeInTheDocument();
-  });
 });
