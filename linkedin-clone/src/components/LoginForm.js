1a1ab522cb248293b2dfa525d56397fccace3309
drbladz <rtahmid14@gmail.com>
2023-04-24 19:03:16 -0400

    CSS improvements for login page

--- linkedin-clone/src/components/LoginForm.js
+++ linkedin-clone/src/components/LoginForm.js
@@ -5,10 +5,10 @@ const LoginForm = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div>
+    <div className="form">
       <input type="email" required={true} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
       <input type="password" required={true} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit" onClick={()=>props.login(email, password)}>Login</button>
+      <button className="apply-btn" type="submit" onClick={()=>props.login(email, password)}>Login</button>
     </div>
   );
 };
